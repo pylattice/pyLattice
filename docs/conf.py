e08222464d43b41a51434677bd9d52edfa1d48ed
Joh Schöneberg <joh@schoeneberglab.org>
2020-04-29 19:05:09 -0700

    add sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyLattice'
-copyright = '2018, Joh Schoeneberg'
+copyright = '2018-2020, Joh Schoeneberg'
 author = 'Joh Schoeneberg'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -84,7 +84,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -168,6 +171,3 @@ texinfo_documents = [
      author, 'pyLattice', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
