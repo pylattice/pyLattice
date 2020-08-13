b3708b1cf82279e10d85814d39e9166f78b83190
Joh Schöneberg <joh@schoeneberglab.org>
2020-08-13 12:12:15 -0700

    get ready for unit-tests and sphinx autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages','sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,8 +48,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyLattice'
-copyright = '2018-2020, Joh Schoeneberg'
-author = 'Joh Schoeneberg'
+copyright = '2018-2020, the pyLattice development team'
+author = 'The pyLattice development team ()'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
